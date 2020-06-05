c478b7bbbab6ee298a25c29c7357783d94aaecaa
Zahary Karadjov <zahary@gmail.com>
2020-06-05 17:06:48 +0300

    Proper JSON serialization for IP addresses

--- json_serialization/std/net.nim
+++ json_serialization/std/net.nim
@@ -1,9 +1,25 @@
 import
-  std/[net, strutils],
+  std/strutils, stew/shims/net,
   ../../json_serialization, chronos/transports/common
 
 export
   net, common
+
+proc writeValue*(writer: var JsonWriter, value: IpAddress) =
+  writeValue(writer, $value)
+
+proc readValue*(reader: var JsonReader, value: var IpAddress) =
+  let s = reader.readValue(string)
+  try:
+    value = parseIpAddress s
+  except CatchableError:
+    raiseUnexpectedValue(reader, "Invalid IP address")
+
+template writeValue*(writer: var JsonWriter, value: ValidIpAddress) =
+  writeValue writer, IpAddress(value)
+
+template readValue*(reader: var JsonReader, value: var ValidIpAddress) =
+  readValue reader, IpAddress(value)
 
 proc writeValue*(writer: var JsonWriter, value: Port) =
   writeValue(writer, uint16 value)
